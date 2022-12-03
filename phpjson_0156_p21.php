51b4c37e0b2f8a0d1e19ae91c4f62106485b590c
63410156 <110089097+63410156@users.noreply.github.com>
2022-12-03 18:13:53 +0700

    Add files via upload

---
+++ phpjson_0156_p21.php
@@ -0,0 +1,4 @@
+<?php
+    $arr2 = array('firstName' => 'Ratchaphumphoug', 'lastName' => 'Phonthai','email' => '63410156@go.buu.ac.th');
+    echo json_encode($arr2);
+?>
---
+++ phpjson_0156_p25.php
@@ -0,0 +1,4 @@
+<?php
+    $json2 = '{"firstName" : "Ratchaphumphoug", "lastName" : "Phonthai", "email" : "63410156@go.buu.ac.th"}';
+    var_dump(json_decode($json2, true));
+?>
